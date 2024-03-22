810886d828046468fb5267670ae32dc227affb98
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-03-22 16:23:11 +0530

    auto list

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,23 @@
+import 'package:animation/model/model.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Activity> activities = [];
+  @override
+  void initState() {
+    activities = [
+      Activity(
+          name: "unknow", location: "kerala,india", imageUrl: "", price: 150)
+    ];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +25,24 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text("Home Page")),
       ),
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: const Color.fromARGB(255, 68, 118, 143),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              "Pick Your \nFavorite Activity ",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
